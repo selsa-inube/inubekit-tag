f7c6198bd180d2b9af9ee13164788cf4a8c2c80e
Andres Babativa <carlos3k11@gmail.com>
2024-02-01 17:47:03 -0500

    feat: udd disableTelemetry and defaultName options to Storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  core: {
+    disableTelemetry: true,
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -13,7 +16,8 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
+    defaultName: "Documentation",
   },
 };
 export default config;

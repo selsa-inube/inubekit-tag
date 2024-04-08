f5d8ed8259ed5bc698fdf16b4f743f05b0a3bbe8
cmarin001 <cmarin6051@gmail.com>
2024-04-08 17:55:26 -0500

    fix(tag): adjustment in order to avoid to use the theme context

--- src/Tag/index.tsx
+++ src/Tag/index.tsx
@@ -1,7 +1,9 @@
 import { StyledTag } from "./styles";
 import { ITagAppearance, ITagWeight } from "./props";
-import { Text } from "@inubekit/text";
+import { ITextAppearance, Text } from "@inubekit/text";
 import { inube } from "@inubekit/foundations";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
 
 interface ITag {
   appearance: ITagAppearance;
@@ -11,15 +13,23 @@ interface ITag {
 
 const Tag = (props: ITag) => {
   const { appearance, weight = "normal", label } = props;
+  const theme: typeof inube = useContext(ThemeContext);
+  const textAppearance = (
+    appearance: ITextAppearance,
+    weight: ITagWeight,
+  ): ITextAppearance => {
+    return (
+      (theme?.tag?.[appearance][weight]?.content
+        ?.appearance as keyof typeof theme.tag) ||
+      inube.tag[appearance][weight].content.appearance
+    );
+  };
 
   return (
     <StyledTag $appearance={appearance} $weight={weight}>
       <Text
         type="label"
-        appearance={
-          inube.tag[appearance][weight].content
-            .appearance as keyof typeof inube.tag
-        }
+        appearance={textAppearance(appearance, weight)}
         size="small"
         textAlign="start"
       >

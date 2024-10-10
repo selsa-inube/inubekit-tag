a01fc3d199cad0ed26e251cb053d362b308ce11b
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 10:55:15 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Tag } from "./Tag";
 export type { ITag } from "./Tag";
 export type { ITagAppearance, ITagWeight } from "./Tag/props";
+export { tokens as TagTokens } from "./Tag/Tokens/tokens";
